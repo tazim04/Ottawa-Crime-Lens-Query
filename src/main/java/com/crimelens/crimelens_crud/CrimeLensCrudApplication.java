f3ef963a08f8f9d5eec9f0f58004a1486e3eca32
tazim04 <35643104+tazim04@users.noreply.github.com>
2026-01-13 13:47:07 -0500

    Init project

---
+++ src/main/java/com/crimelens/crimelens_crud/CrimeLensCrudApplication.java
@@ -0,0 +1,13 @@
+package com.crimelens.crimelens_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrimeLensCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrimeLensCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crimelens/crimelens_crud/CrimeLensCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crimelens.crimelens_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrimeLensCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
