6ca82a812a4ee9c8068a003a2b31424911d03683
tazim04 <35643104+tazim04@users.noreply.github.com>
2026-01-16 22:55:41 -0500

    Fixed grid-stats endpoint

--- src/main/java/com/crimelens/crimelens_query/controller/GridStatsController.java
+++ src/main/java/com/crimelens/crimelens_query/controller/GridStatsController.java
@@ -7,17 +7,20 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/crime")
 @Slf4j
 public class GridStatsController {
 
   private final GridStatsService service;
 
   @GetMapping("/grid-stats")
-  public GridStatDTO getGridStats(@Valid GridStatsRequest request) {
+  public GridStatDTO getGridStats(@Valid @ModelAttribute GridStatsRequest request) {
     log.info("Running /get-grid-stats");
     return service
         .getGridStatsForPoint(request.lon(), request.lat())
